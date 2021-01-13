4957880d58418c3f5e87822c50b5a7a9096f7f84
nicholasdefina <nicholasdefina@gmail.com>
2021-01-12 19:46:22 -0500

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const markup = () => {
+    return (
+        <div>
+            Another page
+            <Link to="/">Return home</Link>
+        </div>
+    );
+}
+export default markup
